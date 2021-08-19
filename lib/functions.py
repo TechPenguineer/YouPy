8e2745a8925164180efdaca8cc99fa3144209dd0
Tech Penguin <85949246+TechPenguineer@users.noreply.github.com>
2021-08-19 19:52:59 -0400

    main setup and api feedback strings

---
+++ lib/functions.py
@@ -0,0 +1,8 @@
+API_FETCH_RESULTS = {
+    'FETCH_ERROR': 'API Token Error: Invalid',
+    'COMPLETED': 'API Token Returns: Success',
+    'EXPIRED': 'API Token Error: Expired'
+}
+
+def setApiToken(TOKEN: str):
+    ...
